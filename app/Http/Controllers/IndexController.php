9aff51006965bbb83b401807ac1c1083ce9eb22f
Arman Abovyan <armani.abo@gmail.com>
2020-02-28 19:03:12 +0400

    'Changes'

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Media;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
-use Pion\Laravel\ChunkUpload\Exceptions\UploadMissingFileException;
-use Pion\Laravel\ChunkUpload\Handler\ContentRangeUploadHandler;
 use Pion\Laravel\ChunkUpload\Handler\HandlerFactory;
 use Pion\Laravel\ChunkUpload\Receiver\FileReceiver;
 
@@ -16,106 +14,54 @@ class IndexController extends Controller
     }
 
     public function insertImages(Request $request) {
-        $data = [];
+        try {
+            $data = [];
+            $files = $request->all();
 
-        $files = $request->allFiles();
+            if (!is_array($files)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'No files'
+                ]);
+            }
 
-        if (!is_array($files)) {
+            foreach ($files as $file) {
+                $receiver = new FileReceiver($file, $request, HandlerFactory::classFromRequest($request));
+
+                if ($receiver->isUploaded()) {
+                    $this->saveFile($receiver->receive()->getFile());
+                    continue;
+                }
+
+                $save = $receiver->receive();
+
+                if ($save->isFinished()) {
+                    $data[] = $this->saveFile($save->getFile());
+                }
+                else {
+                    $handler = $save->handler();
+                    $data[] = [
+                        "progress" => $handler->getPercentageDone(),
+                        "finished" => false
+                    ];
+                }
+            }
+
+            return response()->json([
+                'success' => true
+            ]);
+        } catch (\Exception $e){
             return response()->json([
                 'success' => false,
-                'message' => 'No files'
+                'message' => $e->getMessage()
             ]);
         }
-
-        foreach ($files as $file) {
-            //Вместо того, чтобы передавать имя индекса, передайте объект UploadFile из массива $ files, который мы зацикливаем
-            //Исключение выдается, если загрузка файла недопустима (ограничение размера и т. Д.)
-
-            //Создать приемник файлов через динамический обработчик
-            $receiver = new FileReceiver($file, $request, HandlerFactory::classFromRequest($request));
-
-            if ($receiver->isUploaded()) {
-                continue;
-            }
-
-            //получить файл
-            $save = $receiver->receive();
-
-            // проверить, завершена ли загрузка (в режиме чанков он будет отправлять файлы меньшего размера)
-            if ($save->isFinished()) {
-                // сохраните файл и верните любой нужный вам ответ
-                $files[] = $this->saveFile($save->getFile());
-            } else {
-                // мы находимся в режиме чанка, давайте отправим текущий прогресс
-
-                $handler = $save->handler();
-
-                // Добавить готовый файл
-                $data[] = [
-                    "progress" => $handler->getPercentageDone(),
-                    "finished" => false
-                ];
-            }
-        }
-
-        dd($data);
-//        return response()->json([
-//            'success' => true
-//        ]);
     }
 
-    protected function saveFileToS3($file)
+    protected function saveFile($file)
     {
-        $fileName = $this->createFilename($file);
-
-        $disk = Storage::disk('s3');
-        // It's better to use streaming Streaming (laravel 5.4+)
-        $disk->putFileAs('photos', $file, $fileName);
-
-        // for older laravel
-        // $disk->put($fileName, file_get_contents($file), 'public');
-        $mime = str_replace('/', '-', $file->getMimeType());
-
-        // We need to delete the file when uploaded to s3
-        unlink($file->getPathname());
-
-        return response()->json([
-            'path' => $disk->url($fileName),
-            'name' => $fileName,
-            'mime_type' =>$mime
-        ]);
-    }
-
-    protected function saveFile(UploadedFile $file)
-    {
-        $fileName = $this->createFilename($file);
-        // Group files by mime type
-        $mime = str_replace('/', '-', $file->getMimeType());
-        // Group files by the date (week
-        $dateFolder = date("Y-m-W");
-
-        // Build the file path
-        $filePath = "upload/{$mime}/{$dateFolder}/";
-        $finalPath = storage_path("app/".$filePath);
-
-        // move the file name
-        $file->move($finalPath, $fileName);
-
-        return response()->json([
-            'path' => $filePath,
-            'name' => $fileName,
-            'mime_type' => $mime
-        ]);
-    }
-
-    protected function createFilename(UploadedFile $file)
-    {
-        $extension = $file->getClientOriginalExtension();
-        $filename = str_replace(".".$extension, "", $file->getClientOriginalName()); // Filename without extension
-
-        // Add timestamp hash to name of the file
-        $filename .= "_" . md5(time()) . "." . $extension;
-
-        return $filename;
+        $path = time() . $file->getClientOriginalName();
+        $file->move(public_path('uploads'),$path);
+        Media::query()->create(['path' => $path]);
     }
 }
--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from "react-router-dom"
-import routes from "../routes/routes"
-import Header from "./Header/Header";
+import ImageUploader from 'react-images-upload';
 
-const App = () => {
-    return (
-        <BrowserRouter>
-            <main>
-                <Header/>
-                <Switch>
-                    {routes.map((route, i) => (
-                        <Route
-                            exact={route.exact}
-                            key={'route-' + route.path + '-' + i}
-                            path={route.path}
-                            render={() => <route.component/>}
-                        />
-                    ))}
-                </Switch>
-            </main>
-        </BrowserRouter>
-    );
-};
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { pictures: [] };
+        this.onDrop = this.onDrop.bind(this);
+        this.insert = this.insert.bind(this);
+    }
+
+    onDrop(picture) {
+        this.setState({
+            pictures: this.state.pictures.concat(picture),
+        });
+    }
+
+    insert(){
+        let formData = new FormData();
+        for(let key in this.state.pictures){
+            formData.append(key,this.state.pictures[key]);
+        }
+
+        axios.post('/insert-images', formData).then(({data}) => {
+            if (data.success){
+                alert('Inserted');
+            } else{
+                alert(data.message);
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className='d-flex flex-column align-items-center'>
+                <button onClick={this.insert} className='btn btn-success'>Insert</button>
+                <ImageUploader
+                    withPreview={true}
+                    label={'Max file size: 2GB --- Accepted: cr2|dng|tiff|jpg|png|rar'}
+                    withIcon={true}
+                    buttonText='Choose files'
+                    onChange={this.onDrop}
+                    imgExtension={['cr2', 'dng', 'tiff', 'jpg', 'png', 'rar']}
+                    maxFileSize={2048000000}
+                />
+            </div>
+        );
+    }
+}
 
 export default App;
